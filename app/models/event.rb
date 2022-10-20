982d0877359354d47b36aba537bb566ce258d30c
funkyman3121 <alvinmasaba@gmail.com>
2022-10-20 13:10:12 -0400

    Adds validations to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,9 @@
 class Event < ApplicationRecord
+  validates :name, :date, :location, presence: true
+
   belongs_to :creator, class_name: 'User'
-
   has_many :event_attendees, foreign_key: :attended_event_id
-
   has_many :attendees, through: :event_attendees, source: :event_attendee
-
   has_many :invites
   
   default_scope { order(date: :desc)}
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event'
-
   has_many :event_attendees, foreign_key: :event_attendee_id
-
   has_many :attended_events, through: :event_attendees, source: :attended_event
-
   has_many :invites
 
   def is_attending?(event)
